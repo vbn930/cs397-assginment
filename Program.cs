d39a7e3a56dfb682f6ec2943e4559282ce26b515
d.lee <d.lee@digipen.edu>
2025-01-26 23:06:33 -0800

    Add hellojason endpoint

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
+public class HelloMessage
+{
+    public string Message { get; set; } = string.Empty;
+}
+
 class Program
 {
     private static async Task HelloWorldDelegate(HttpContext context){
+        Console.WriteLine("Hello Called");
         await context.Response.WriteAsync("Hello World!");
     }
 
     private static async Task GoodbyeDelegate(HttpContext context){
+        Console.WriteLine("Goodbye Called");
         await context.Response.WriteAsync("Goodbye World!");
+    }
+
+    private static async Task HelloJsonDelegate(HttpContext context){
+        HttpRequest request = context.Request;
+
+        string echostring = "Hello, World!";
+
+        if (request.Query.TryGetValue("Message", out StringValues messageValues))
+        {
+            echostring = messageValues[0];
+        }
+
+        Console.WriteLine(echostring);
+
+        await context.Response.WriteAsync(echostring);
     }
 
     static void Main(String[] args){
@@ -15,6 +37,7 @@ class Program
         app.MapGet("/", HelloWorldDelegate);
         app.MapGet("/hello", HelloWorldDelegate);
         app.MapGet("/goodbye", GoodbyeDelegate);
+        app.MapGet("/hellojson", HelloJsonDelegate);
 
         app.Run();
     }
